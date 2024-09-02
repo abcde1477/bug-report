ff752bac55e408566240d7cae3d2eed27a9e701f
abcde1477 <130267492+abcde1477@users.noreply.github.com>
2024-09-02 15:23:38 +0800

    Add files via upload

---
+++ naive-ui-2-39/README.md
@@ -0,0 +1,5 @@
+### bug reproduce
+
+遇到一个bug, dropdown无法正常展示多个多级菜单, 这里复现了一级菜单含有多个带子菜单的情况
+1. npm install
+2. npm run dev
---
+++ naive-ui-2-39/src/App.vue
@@ -0,0 +1,99 @@
+<script setup>
+  import {NDropdown,NButton} from 'naive-ui'
+  const options = [
+    {
+      label: "杰·盖茨比",
+      key: "jay gatsby"
+    },
+    {
+      type: "divider",
+      key: "d1"
+    },
+    {
+      label: "尼克·卡拉威",
+      key: "nick carraway"
+    },
+    {
+      label: "第一级菜单1",
+      key: "others1",
+      children: [
+        {
+          label: "乔丹·贝克",
+          key: "jordan baker"
+        },
+        {
+          label: "汤姆·布坎南",
+          key: "tom buchanan"
+        }
+      ]
+    },
+/*    {
+      label: "第一级菜单2",
+      key: "others2",
+      children: [
+        {
+          label: "乔丹·贝克",
+          key: "jordan baker"
+        },
+        {
+          label: "汤姆·布坎南",
+          key: "tom buchanan"
+        }
+      ]
+    },
+    {
+      label: "第一级菜单3",
+      key: "others3",
+      children: [
+        {
+          label: "乔丹·贝克",
+          key: "jordan baker"
+        },
+        {
+          label: "汤姆·布坎南",
+          key: "tom buchanan"
+        }
+      ]
+    },*/
+    {
+      label: "尼克1",
+      key: "nick1"
+    },
+    {
+      label: "尼克2",
+      key: "nick2"
+    },
+
+    {
+      label: "尼克3",
+      key: "nick3"
+    },
+
+    {
+      label: "第一级菜单4",
+      key: "others4",
+      children: [
+        {
+          label: "乔丹·贝克",
+          key: "jordan baker"
+        },
+        {
+          label: "汤姆·布坎南",
+          key: "tom buchanan"
+        }
+      ]
+    },
+  ];
+  const handleSelect = (value)=>{
+    console.log(value);
+  }
+</script>
+
+<template>
+  <n-dropdown trigger="click" :options="options" placement="top" @select="handleSelect">
+    <n-button>点击！</n-button>
+  </n-dropdown>
+</template>
+
+<style scoped>
+</style>

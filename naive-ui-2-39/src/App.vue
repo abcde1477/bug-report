2d59c7139667d15de1315e2b2e309e6021805bbe
abcde1477 <130267492+abcde1477@users.noreply.github.com>
2024-10-12 23:05:32 +0800

    Update App.vue
    Modify the options to better show the issue.

--- naive-ui-2-39/src/App.vue
+++ naive-ui-2-39/src/App.vue
@@ -1,89 +1,74 @@
 <script setup>
   import {NDropdown,NButton} from 'naive-ui'
   const options = [
-    {
-      label: "杰·盖茨比",
-      key: "jay gatsby"
-    },
-    {
-      type: "divider",
-      key: "d1"
-    },
-    {
-      label: "尼克·卡拉威",
-      key: "nick carraway"
-    },
-    {
-      label: "第一级菜单1",
-      key: "others1",
-      children: [
-        {
-          label: "乔丹·贝克1",
-          key: "jordan baker"
-        },
-        {
-          label: "汤姆·布坎南1",
-          key: "tom buchanan"
-        }
-      ]
-    },
-/*    {
-      label: "第一级菜单2",
-      key: "others2",
-      children: [
-        {
-          label: "乔丹·贝克",
-          key: "jordan baker"
-        },
-        {
-          label: "汤姆·布坎南",
-          key: "tom buchanan"
-        }
-      ]
-    },
-    {
-      label: "第一级菜单3",
-      key: "others3",
-      children: [
-        {
-          label: "乔丹·贝克",
-          key: "jordan baker"
-        },
-        {
-          label: "汤姆·布坎南",
-          key: "tom buchanan"
-        }
-      ]
-    },*/
-    {
-      label: "尼克1",
-      key: "nick1"
-    },
-    {
-      label: "尼克2",
-      key: "nick2"
-    },
+  {
+    label: "杰·盖茨比",
+    key: "jay gatsby",
+  },
+  {
+    type: "divider",
+    key: "d1",
+  },
+  {
+    label: "尼克·卡拉威",
+    key: "nick carraway",
+  },
+  {
+    label: "第一级菜单1",
+    key: "others1",
+    children: [
+      {
+        label: "乔丹·贝克1",
+        key: "jordan baker",
+      },
+      {
+        label: "汤姆·布坎南1",
+        key: "tom buchanan",
+      },
+    ],
+  },
+  {
+    label: "尼克1",
+    key: "nick1",
+  },
+  {
+    label: "尼克2",
+    key: "nick2",
+  },
+  {
+    label: "第一级菜单3",
+    key: "others3",
+    children: [
+      {
+        label: "乔丹·贝克",
+        key: "jordan baker",
+      },
+      {
+        label: "汤姆·布坎南",
+        key: "tom buchanan",
+      },
+    ],
+  },
+  {
+    label: "尼克3",
+    key: "nick3",
+  },
 
-    {
-      label: "尼克3",
-      key: "nick3"
-    },
-
-    {
-      label: "第一级菜单4",
-      key: "others4",
-      children: [
-        {
-          label: "乔丹·贝克2",
-          key: "jordan baker"
-        },
-        {
-          label: "汤姆·布坎南2",
-          key: "tom buchanan"
-        }
-      ]
-    },
-  ];
+  {
+    label: "第一级菜单4",
+    key: "others4",
+    children: [
+      {
+        label: "乔丹·贝克2",
+        key: "jordan baker",
+      },
+      {
+        label: "汤姆·布坎南2",
+        key: "tom buchanan",
+      },
+    ],
+  },
+];
   const handleSelect = (value)=>{
     console.log(value);
   }
